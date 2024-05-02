1cd2c6979d663153a6273c38486ea93af5c3b006
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-05-02 10:33:46 +0600

    google call back url

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "https://sparknex-backend.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
